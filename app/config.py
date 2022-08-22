7d4f273423857b75ee365c05b575a6e671f0e769
Rykov7 <rykov7@protonmail.ch>
2022-08-22 22:15:48 +0300

    hide links in config

--- app/config.py
+++ app/config.py
@@ -7,5 +7,5 @@ load_dotenv()
 TOKEN = os.environ.get('LUTZPYBOT', 'Token not in ENVIRON')
 bot = TeleBot(TOKEN)
 
-ADMIN_ID = 280887861  # t.me/Rykov7
-PYTHONCHATRU = -1001338616632  # t.me/pythonchatru
+ADMIN_ID = 280887861  # Rykov7
+PYTHONCHATRU = -1001338616632  # pythonchatru
