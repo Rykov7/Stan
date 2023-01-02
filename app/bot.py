7d6353f41a6b5bdd82bf43474dcf94fb132d8a12
Rykov7 <rykov7@protonmail.ch>
2023-01-03 02:10:51 +0300

    add /remove

--- app/bot.py
+++ app/bot.py
@@ -272,7 +272,20 @@ def add_stan_quote(message):
                 bot.send_message(message.chat.id, f'✅ <b>Добавил</b>\n  └ <i>{message.reply_to_message.text}</i>')
             else:
                 bot.send_message(message.chat.id,
-                                 f'⛔️ <b><u>Не добавил</u></b>, есть токое\n  └ <i>{message.reply_to_message.text}</i>')
+                                 f'⛔️ <b>Не добавил</b>, есть токое\n  └ <i>{message.reply_to_message.text}</i>')
+
+
+@bot.message_handler(func=is_white, commands=['remove'])
+def remove_stan_quote(message):
+    if message.reply_to_message and message.reply_to_message.text:
+        if message.reply_to_message.text in (i.rstrip() for i in open('Stan.txt', 'r', encoding='utf8')):
+            quotes = [i.rstrip() for i in open('Stan.txt', 'r', encoding='utf8')]
+            with open('Stan.txt', 'w', encoding='utf8') as stan_quotes:
+                quotes.remove(message.reply_to_message.text)
+                stan_quotes.writelines(quotes)
+            bot.send_message(message.chat.id, f'✅ <b>Удолил</b>\n  └ <i>{message.reply_to_message.text}</i>')
+        else:
+            bot.send_message(message.chat.id, f'⛔️ <b>Нет такого</b>\n  └ <i>{message.reply_to_message.text}</i>')
 
 
 @bot.message_handler(func=is_admin, commands=['stats'])
