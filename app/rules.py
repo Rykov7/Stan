32b63b35105760dc54e1672ca27fdcf771367702
Rykov7 <rykov7@protonmail.ch>
2023-01-23 17:58:12 +0300

    improve rules

--- app/rules.py
+++ app/rules.py
@@ -6,7 +6,8 @@ html = BeautifulSoup(urlopen('https://telegra.ph/pythonchatru-07-07'), 'lxml')
 
 def get_rule(li):
     li = int(li)
-    try:
-        return html.find_all('li')[li - 1].text
-    except IndexError:
-        return 'Нет такого правила.'
+    lis = html.find_all('li')
+    if li <= len(lis):
+        return lis[li - 1].text
+    else:
+        return f'Пока не придумали.'
