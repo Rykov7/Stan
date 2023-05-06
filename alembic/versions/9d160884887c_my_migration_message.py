c29bddd1e7dfdda9453b88540a3e0c73857ede49
Rykov7 <rykov7@protonmail.ch>
2023-05-06 09:33:31 +0300

    Add .gitignore

---
+++ alembic/versions/9d160884887c_my_migration_message.py
@@ -0,0 +1,24 @@
+"""My migration message
+
+Revision ID: 9d160884887c
+Revises: 
+Create Date: 2023-04-06 00:47:17.551695
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '9d160884887c'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
