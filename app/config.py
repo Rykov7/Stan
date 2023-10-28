1bac7420e93532f0f140c01f756023dab150c098
Rykov7 <rykov7@protonmail.ch>
2023-10-29 00:02:21 +0300

    Lower bot log level

--- app/config.py
+++ app/config.py
@@ -8,10 +8,8 @@ from telebot.async_telebot import AsyncTeleBot
 from telebot import logger, types
 from dotenv import load_dotenv
 
-logging.basicConfig(
-    level=logging.INFO, format="%(asctime)s %(message)s", datefmt="%Y-%m-%d %H:%M:%S"
-)
-logger.setLevel(logging.INFO)
+logging.basicConfig(level=logging.INFO, format="%(asctime)s %(message)s", datefmt="%Y-%m-%d %H:%M:%S")
+logger.setLevel(logging.ERROR)
 
 logging.warning("[START] Stan")
 
